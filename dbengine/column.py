53755c6d1add8f9ae9140949a59c064c439a8a99
semen603089 <semen603089@mail.ru>
2022-05-24 02:38:05 +0300

    column get, del, alt added

--- dbengine/column.py
+++ dbengine/column.py
@@ -49,11 +49,14 @@ def get_column(
 ) -> Tuple[DbColumn, DbColumnAttributes]:
     """Get last version of column in table in branch"""
     logging.debug('get_column')
+    s = session.query(Commit).filter(Commit.branch_id == branch.id).order(Commit.id.desc()).first()
+    return session.query([DbColumn, DbColumnAttributes]).filter(DbColumn.id == s.attribute_id_out)
 
 
 def update_column(
         branch: Branch,
         table: DbTable,
+        column: DbColumn,
         *,
         name: Optional[str] = None,
         datatype: Optional[str] = None
@@ -63,12 +66,52 @@ def update_column(
     То есть создать новые атрибуты и указать начало и конец
     """
     logging.debug('update_column')
+    if branch.type != BranchTypes.MAIN:
+        s = session.query(Commit).filter(Commit.branch_id == branch.id).order_by(Commit.id.desc()).first()
+        new_commit = Commit()
+        new_commit.branch_id = branch.id
+        if s:
+            new_commit.prev_commit_id = s.id
+        new_commit.attribute_id_in = column.id
+        new_column = DbColumn()
+        new_column.type = "COLUMN"
+        new_column.table_id = table.id
+        session.add(new_column)
+        session.flush()
+        session.commit()
+        new_column_attribute = DbColumnAttributes()
+        new_column_attribute.type = "COLUMN"
+        new_column_attribute.id = new_column.id
+        new_column_attribute.datatype = datatype
+        new_column_attribute.name = name
+        session.add(new_column_attribute)
+        session.flush()
+        session.commit()
+        new_commit.attribute_id_out = new_column.id
+        session.add(new_commit)
+        session.flush()
+        session.commit()
+    else:
+        raise ProhibitedActionInBranch("Column altering", branch.name)
+    return new_column, new_column_attribute, new_commit
 
 
-def delete_column(branch: Branch, table: DbTable):
+def delete_column(branch: Branch, column: DbColumn):
     """Delete column from table from branch
 
     То есть надо удалить у колонки атрибуты, сам объект колонки останется
     То есть создать коммит с пустым концом
     """
     logging.debug('delete_column')
+    if branch.type != BranchTypes.MAIN:
+        s = session.query(Commit).filter(Commit.branch_id == branch.id).order_by(
+            Commit.id.desc()).first()
+        new_commit = Commit()
+        new_commit.branch_id = branch.id
+        if s:
+            new_commit.prev_commit_id = s.id
+        new_commit.attribute_id_in = column.table_id
+        new_commit.attribute_id_out = None
+        session.add(new_commit)
+        session.flush()
+        session.commit()
