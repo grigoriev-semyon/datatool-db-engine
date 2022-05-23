98eba2c61e78cbb2e2abe8747cce345ff09638ab
semen603089 <semen603089@mail.ru>
2022-05-24 00:55:54 +0300

    branch added

--- dbengine/__main__.py
+++ dbengine/__main__.py
@@ -1,28 +1,8 @@
-# import database
+import logging
+logging.basicConfig(level=logging.DEBUG, format="\x1b[33;20m%(asctime)s %(levelname)s %(module)s:%(funcName)s %(message)s\x1b[0m")
 
-# database.create_tables()
+from .models import create_tables
+from .branch import create_branch
 
-# br1 = database.get_branch(database.create_new_branch("Second branch"))
-# print(br1)
-# database.ok_branch_creator_table(br1, "table1")
-# database.ok_branch_creator_column(br1, "col1")
-# database.ok_branch_creator_column(br1, "col2")
-# database.ok_branch_creator_column(br1, "col3")
-# database.ok_branch_creator_column(br1, "col4")
-
-# database.ok_branch_creator_table(br1, "table2")
-
-
-# database.create_column('testcolumn', 'COLUMN')
-# database.create_tables()
-# database.create_new_branch("test5")
-# database.create_new_branch("test6")
-# database.create_new_branch("test4")
-# print(database.get_branch(2))
-# database.ok_branch_creator_column(database.get_branch(2), "creator works")
-# database.ok_branch_deleter_column(database.get_branch(4), 2)
-# #database.ok_branch_changer_column(database.get_branch(3), "DOES CHANGER WORKS??/", 2)
-# database.ok_branch_creator_table(database.get_branch(5), "hehh")
-# database.ok_branch_alter_table(database.get_branch(5), "chander table works", 5)
-# database.delete_table(database.get_branch(6), 5)
-# database.merge(database.get_branch(5), "MERGED")
+create_tables()
+create_branch('b1')
--- dbengine/branch.py
+++ dbengine/branch.py
@@ -1,7 +1,19 @@
 import logging
 from typing import Union
 
-from .models import Branch
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from .models import Branch, BranchTypes, Commit
+from .settings import Settings
+from exceptions import ProhibitedActionInBranch
+
+settings = Settings()
+
+engine = create_engine(settings.DB_DSN, echo=True)
+engine.connect()
+Session = sessionmaker(bind=engine)
+session = Session()
 
 logger = logging.getLogger(__name__)
 
@@ -11,7 +23,23 @@ def init_main() -> Branch:
 
     Тип ветки MAIN, название ветки Main
     """
+    s = session.query(Branch).all()
+    if not s:
+        new_branch = Branch()
+        new_branch.name = "MAIN BRANCH"
+        new_branch.type = BranchTypes.MAIN
+        session.add(new_branch)
+        session.flush()
+        session.commit()
+        new_commit = Commit()
+        new_commit.branch_id = new_branch.id
+        session.add(new_commit)
+        session.flush()
+        session.commit()
+    else:
+        raise Exception
     logger.debug("init_main")
+    return new_branch
 
 
 def create_branch(name) -> Branch:
@@ -19,7 +47,23 @@ def create_branch(name) -> Branch:
 
     Тип ветки WIP, название ветки `name`
     """
+    s = session.query(Branch).all()
+    if s:
+        new_branch = Branch()
+        new_branch.name = name
+        new_branch.type = BranchTypes.WIP
+        session.add(new_branch)
+        session.flush()
+        session.commit()
+        new_commit = Commit()
+        new_commit.branch_id = new_branch.id
+        session.add(new_commit)
+        session.flush()
+        session.commit()
+    else:
+        raise Exception
     logger.debug("create_branch")
+    return new_branch
 
 
 def request_merge_branch(branch: Branch) -> Branch:
@@ -27,7 +71,15 @@ def request_merge_branch(branch: Branch) -> Branch:
 
     Только если сейчас WIP
     """
+    if branch.type == BranchTypes.WIP:
+        branch.type = BranchTypes.MERGED
+        session.query(Branch).filter(Branch.id == branch.id).update({"type": BranchTypes.MERGED})
+        session.flush()
+        session.commit()
+    else:
+        raise Exception
     logger.debug("request_merge_branch")
+    return branch
 
 
 def unrequest_merge_branch(branch: Branch) -> Branch:
@@ -35,7 +87,13 @@ def unrequest_merge_branch(branch: Branch) -> Branch:
 
     Только если сейчас MR
     """
+    if branch.type == BranchTypes.MR:
+        branch.type = BranchTypes.WIP
+        session.query(Branch).filter(Branch.id == branch.id).update({"type": BranchTypes.WIP})
+        session.flush()
+        session.commit()
     logger.debug("unrequest_merge_branch")
+    return branch
 
 
 def ok_branch(branch: Branch) -> Branch:
@@ -43,9 +101,17 @@ def ok_branch(branch: Branch) -> Branch:
 
     Только если сейчас MR
     """
+    if branch.type == BranchTypes.MR:
+        branch.type = BranchTypes.MERGED
+        session.query(Branch).filter(Branch.id == branch.id).update({"type": BranchTypes.MERGED})
+        session.flush()
+        session.commit()
     logger.debug("ok_branch")
+    return branch
 
 
-def get_branch(name_or_id: Union[str, int]) -> Branch:
+def get_branch(id: int) -> Branch:
     """Return branch by id or name"""
     logger.debug("get_branch")
+    return session.query(Branch).filter(Branch.id == id).one()
+
