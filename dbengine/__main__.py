f45b783f1d639ce3b4367fed912a60cb2b705e23
semen603089 <semen603089@mail.ru>
2022-05-24 22:36:40 +0300

    fix

--- dbengine/__main__.py
+++ dbengine/__main__.py
@@ -1,5 +1,7 @@
 import logging
-logging.basicConfig(level=logging.DEBUG, format="\x1b[33;20m%(asctime)s %(levelname)s %(module)s:%(funcName)s %(message)s\x1b[0m")
+
+logging.basicConfig(level=logging.DEBUG,
+                    format="\x1b[33;20m%(asctime)s %(levelname)s %(module)s:%(funcName)s %(message)s\x1b[0m")
 
 from .models import create_tables
 from .branch import *
@@ -8,9 +10,9 @@ from .column import *
 
 # create_tables()
 # create_main_branch()
-#create_branch('test1')
-#create_table(get_branch(2), 'testtable1')
-#create_table(get_branch(2), 'testtable2')
+# create_branch('test1')
+# create_table(get_branch(2), 'testtable1')
+# create_table(get_branch(2), 'testtable2')
 
 # update_table(get_branch(2), get_table(get_branch(2), 1)[0], "test_update2")
 # delete_table(get_branch(2), get_table(get_branch(2), 1)[0])
@@ -18,4 +20,8 @@ from .column import *
 # create_branch('test2')
 # create_table(get_branch(3), 'testtable3')
 # create_table(get_branch(3), 'testtable4')
-create_column(get_branch(3), get_table(get_branch(3), 5)[0], name="testcolumn", datatype="testtype")
+# create_column(get_branch(3), get_table(get_branch(3), 5)[0], name="testcolumn", datatype="testtype")
+update_column(get_branch(3), get_table(get_branch(3), 6)[0],
+              get_column(get_branch(3), get_table(get_branch(3), 5)[0], 7)[0], name="testupdate",
+              datatype="testupdatetype")
+# create_column(get_branch(3), get_table(get_branch(3), 5)[0], name="here")
