55486bad0b71e63be0eaa66a02784a01881f2853
semen603089 <semen603089@mail.ru>
2022-05-27 00:15:56 +0300

    Noresfound tryexc added

--- dbengine/column.py
+++ dbengine/column.py
@@ -17,31 +17,34 @@ def create_column(
 ) -> Tuple[DbColumn, DbColumnAttributes, Commit]:
     """Create column in table in branch"""
     logging.debug('create_column')
-    if branch.type == BranchTypes.MAIN:
-        raise ProhibitedActionInBranch("Column creating", branch.name)
-    s = session.query(Commit).filter(Commit.branch_id == branch.id).order_by(Commit.id.desc()).first()
-    new_commit = Commit()
-    new_commit.branch_id = branch.id
-    if s:
-        new_commit.prev_commit_id = s.id
-    new_commit.attribute_id_in = None
-    new_column = DbColumn()
-    new_column.type = AttributeTypes.COLUMN
-    new_column.table_id = table.id
-    session.add(new_column)
-    session.flush()
-    new_column_attribute = DbColumnAttributes()
-    new_column_attribute.type = AttributeTypes.COLUMN
-    new_column_attribute.column_id = new_column.id
-    new_column_attribute.datatype = datatype
-    new_column_attribute.name = name
-    session.add(new_column_attribute)
-    session.flush()
-    new_commit.attribute_id_out = new_column.id
-    session.add(new_commit)
-    session.flush()
-    session.commit()
-    return new_column, new_column_attribute, new_commit
+    try:
+        if branch.type == BranchTypes.MAIN:
+            raise ProhibitedActionInBranch("Column creating", branch.name)
+        s = session.query(Commit).filter(Commit.branch_id == branch.id).order_by(Commit.id.desc()).first()
+        new_commit = Commit()
+        new_commit.branch_id = branch.id
+        if s:
+            new_commit.prev_commit_id = s.id
+        new_commit.attribute_id_in = None
+        new_column = DbColumn()
+        new_column.type = AttributeTypes.COLUMN
+        new_column.table_id = table.id
+        session.add(new_column)
+        session.flush()
+        new_column_attribute = DbColumnAttributes()
+        new_column_attribute.type = AttributeTypes.COLUMN
+        new_column_attribute.column_id = new_column.id
+        new_column_attribute.datatype = datatype
+        new_column_attribute.name = name
+        session.add(new_column_attribute)
+        session.flush()
+        new_commit.attribute_id_out = new_column.id
+        session.add(new_commit)
+        session.flush()
+        session.commit()
+        return new_column, new_column_attribute, new_commit
+    except AttributeError:
+        logging.error(AttributeError, exc_info=True)
 
 
 def get_column(
@@ -71,31 +74,34 @@ def update_column(
     То есть создать новые атрибуты и указать начало и конец
     """
     logging.debug('update_column')
-    if branch.type == BranchTypes.MAIN:
-        raise ProhibitedActionInBranch("Column altering", branch.name)
-    s = session.query(Commit).filter(Commit.branch_id == branch.id).order_by(Commit.id.desc()).first()
-    new_commit = Commit()
-    new_commit.branch_id = branch.id
-    if s:
-        new_commit.prev_commit_id = s.id
-    new_commit.attribute_id_in = column.id
-    new_column = DbColumn()
-    new_column.type = AttributeTypes.COLUMN
-    new_column.table_id = table.id
-    session.add(new_column)
-    session.flush()
-    new_column_attribute = DbColumnAttributes()
-    new_column_attribute.type = AttributeTypes.COLUMN
-    new_column_attribute.column_id = new_column.id
-    new_column_attribute.datatype = datatype
-    new_column_attribute.name = name
-    session.add(new_column_attribute)
-    session.flush()
-    new_commit.attribute_id_out = new_column.id
-    session.add(new_commit)
-    session.flush()
-    session.commit()
-    return new_column, new_column_attribute, new_commit
+    try:
+        if branch.type == BranchTypes.MAIN:
+            raise ProhibitedActionInBranch("Column altering", branch.name)
+        s = session.query(Commit).filter(Commit.branch_id == branch.id).order_by(Commit.id.desc()).first()
+        new_commit = Commit()
+        new_commit.branch_id = branch.id
+        if s:
+            new_commit.prev_commit_id = s.id
+        new_commit.attribute_id_in = column.id
+        new_column = DbColumn()
+        new_column.type = AttributeTypes.COLUMN
+        new_column.table_id = table.id
+        session.add(new_column)
+        session.flush()
+        new_column_attribute = DbColumnAttributes()
+        new_column_attribute.type = AttributeTypes.COLUMN
+        new_column_attribute.column_id = new_column.id
+        new_column_attribute.datatype = datatype
+        new_column_attribute.name = name
+        session.add(new_column_attribute)
+        session.flush()
+        new_commit.attribute_id_out = new_column.id
+        session.add(new_commit)
+        session.flush()
+        session.commit()
+        return new_column, new_column_attribute, new_commit
+    except AttributeError:
+        logging.error(AttributeError, exc_info=True)
 
 
 def delete_column(branch: Branch, column: DbColumn):
@@ -105,16 +111,19 @@ def delete_column(branch: Branch, column: DbColumn):
     То есть создать коммит с пустым концом
     """
     logging.debug('delete_column')
-    if branch.type == BranchTypes.MAIN:
-        raise ProhibitedActionInBranch("Column deleting", branch.name)
-    s = session.query(Commit).filter(Commit.branch_id == branch.id).order_by(
-        Commit.id.desc()).first()
-    new_commit = Commit()
-    new_commit.branch_id = branch.id
-    if s:
-        new_commit.prev_commit_id = s.id
-    new_commit.attribute_id_in = column.table_id
-    new_commit.attribute_id_out = None
-    session.add(new_commit)
-    session.flush()
-    session.commit()
+    try:
+        if branch.type == BranchTypes.MAIN:
+            raise ProhibitedActionInBranch("Column deleting", branch.name)
+        s = session.query(Commit).filter(Commit.branch_id == branch.id).order_by(
+            Commit.id.desc()).first()
+        new_commit = Commit()
+        new_commit.branch_id = branch.id
+        if s:
+            new_commit.prev_commit_id = s.id
+        new_commit.attribute_id_in = column.table_id
+        new_commit.attribute_id_out = None
+        session.add(new_commit)
+        session.flush()
+        session.commit()
+    except AttributeError:
+        logging.error(AttributeError, exc_info=True)
