d3c00dd1fbfd61f3bd5c645513dc77063457b407
Roman Dyakov <dyakovri@gmail.com>
2022-05-29 02:01:40 +0300

    test_column_update fix

--- tests/test_table.py
+++ tests/test_table.py
@@ -1,4 +1,5 @@
 import pytest
+from sqlalchemy import column
 from dbengine import *
 from dbengine.exceptions import TableDoesntExists, ColumnDoesntExists
 from . import Session
@@ -81,23 +82,6 @@ def test_column_get():
     assert attrs_name == uattrs.name
 
 
-def test_column_update():
-    session = Session()
-    branch = create_branch("Test Table 1", session=session)
-    table, _, _ = create_table(branch, "test_table_1", session=session)
-    col_1, col_attr_1, _ = create_column(branch, table, name="id", datatype="string", session=session)
-    ucol_1, ucol_attr_1, ucommit_1 = update_column(branch, table, name="uid", datatype="integer", session=session)
-
-    assert col_1.id == ucol_1.id
-    assert ucommit_1.attribute_id_in == col_attr_1.id
-    assert ucommit_1.attribute_id_out == ucol_attr_1.id
-
-    tcol, tcol_attr = get_column(branch, col_1.id, session=session)
-    assert tcol.id == ucol_1.id
-    assert tcol_attr.name == "uid"
-    assert tcol_attr.datatype == "integer"
-
-
 def test_table_update():
     session = Session()
     branch = create_branch("Test Table 1", session=session)
@@ -108,3 +92,20 @@ def test_table_update():
     assert table.id == utable.id
     assert utable_commit.attribute_id_in == table_attr.id
     assert utable_commit.attribute_id_out == utable_attr.id
+
+
+def test_column_update():
+    session = Session()
+    branch = create_branch("Test Table 1", session=session)
+    table, _, _ = create_table(branch, "test_table_1", session=session)
+    col_1, col_attr_1, _ = create_column(branch, table, name="id", datatype="string", session=session)
+    ucol_1, ucol_attr_1, ucommit_1 = update_column(branch, col_1, name="uid", datatype="integer", session=session)
+
+    assert col_1.id == ucol_1.id
+    assert ucommit_1.attribute_id_in == col_attr_1.id
+    assert ucommit_1.attribute_id_out == ucol_attr_1.id
+
+    tcol, tcol_attr = get_column(branch, col_1.id, session=session)
+    assert tcol.id == ucol_1.id
+    assert tcol_attr.name == "uid"
+    assert tcol_attr.datatype == "integer"
