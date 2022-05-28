c2112ea7db2c3f060cd31185ecbfdad42b586aab
Roman Dyakov <dyakovri@gmail.com>
2022-05-29 01:39:00 +0300

    Get tests

--- tests/test_table.py
+++ tests/test_table.py
@@ -1,4 +1,6 @@
+import pytest
 from dbengine import *
+from dbengine.exceptions import TableDoesntExists, ColumnDoesntExists
 from . import Session
 
 
@@ -21,6 +23,64 @@ def test_create():
     assert commit_1.attribute_id_out == col_attr_1.id
 
 
+def test_table_get():
+    session = Session()
+    branch = create_branch("Test Table 1", session=session)
+    branch2 = create_branch("Test Table 3", session=session)
+    table, attrs, _ = create_table(branch, "test_table_1", session=session)
+    create_column(branch, table, name="id", datatype="string", session=session)
+    request_merge_branch(branch, session=session)
+    ok_branch(branch, session=session)
+    table_id = table.id
+    attrs_id = attrs.id
+    attrs_table_id = attrs.table_id
+    attrs_name = attrs.name
+    session.commit()
+
+    # Тут должно падать потому, что ветка была сделана в момент до того, как колонка была создана
+    with pytest.raises(TableDoesntExists):
+        get_table(branch2, table_id, session=session)
+
+    usession = Session()
+    ubranch = create_branch("Test Table 2", session=usession)
+    # FIXME: В ветке не существует таблицы. Но эта таблица существует в main ветке и которая бранчуется позже
+    utable, uattrs = get_table(ubranch, table_id, session=usession)
+    usession.commit()
+
+    assert table_id == utable.id
+    assert attrs_id != uattrs.id, "Commit in branch and in main are different"
+    assert attrs_table_id == uattrs.table_id
+    assert attrs_name == uattrs.name
+
+
+def test_column_get():
+    session = Session()
+    branch = create_branch("Test Table 1", session=session)
+    branch2 = create_branch("Test Table 3", session=session)
+    table, _, _ = create_table(branch, "test_table_1", session=session)
+    col, attrs, _ = create_column(branch, table, name="id", datatype="string", session=session)
+    request_merge_branch(branch, session=session)
+    ok_branch(branch, session=session)
+    col_id = col.id
+    attrs_id = attrs.id
+    attrs_column_id = attrs.column_id
+    attrs_name = attrs.name
+
+    # Тут должно падать потому, что ветка была сделана в момент до того, как колонка была создана
+    with pytest.raises(ColumnDoesntExists):
+        get_column(branch2, col_id, session=session)
+
+    # Тут колонка есть, потому что в мэйн уже изменения завезли
+    ubranch = create_branch("Test Table 2", session=session)
+    # FIXME: В ветке не существует колонки. Но эта колонке существует в main ветке и которая бранчуется позже
+    ucol, uattrs = get_column(ubranch, col_id, session=session)
+
+    assert col_id == ucol.id
+    assert attrs_id != uattrs.id, "Commit in branch and in main are different"
+    assert attrs_column_id == uattrs.column_id
+    assert attrs_name == uattrs.name
+
+
 def test_column_update():
     session = Session()
     branch = create_branch("Test Table 1", session=session)
@@ -36,7 +96,6 @@ def test_column_update():
     assert tcol.id == ucol_1.id
     assert tcol_attr.name == "uid"
     assert tcol_attr.datatype == "integer"
-
 
 
 def test_table_update():
