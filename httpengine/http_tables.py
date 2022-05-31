09a84fc03fc006b4056b4b11cbd053b558ea31c9
semen603089 <semen603089@mail.ru>
2022-05-31 08:03:50 +0300

    fix

--- httpengine/http_tables.py
+++ httpengine/http_tables.py
@@ -1,23 +1,23 @@
 from fastapi import APIRouter
 
-table_router = APIRouter(prefix="/table")
+table_router = APIRouter(prefix="/{branch_id}/table")
 
 
-@table_router.post("/{branch_id}/create/{table_name}")
+@table_router.post("//create/{table_name}")
 async def http_create_table(branch_id: int, table_name: str):
     pass
 
 
-@table_router.get("/{branch_id}/get/{table_id}")
+@table_router.get("/get/{table_id}")
 async def http_get_table(branch_id: int, table_id: int):
     pass
 
 
-@table_router.patch("/{branch_id}/update/{table_id}/{name}")
+@table_router.patch("/update/{table_id}/{name}")
 async def http_update_table(branch_id: int, table_id: int, name: str):
     pass
 
 
-@table_router.delete("/{branch_id}/delete/{table_id}")
+@table_router.delete("/delete/{table_id}")
 async def http_delete_table(branch_id: int, table_id: int):
     pass
