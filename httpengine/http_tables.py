66e46b69b39ceef0855647b95f2dde62ec5fb377
semen603089 <semen603089@mail.ru>
2022-05-31 07:41:46 +0300

    tables added

---
+++ httpengine/http_tables.py
@@ -0,0 +1,25 @@
+from fastapi import APIRouter
+
+from dbengine.models import Branch
+
+table_router = APIRouter(prefix="/table")
+
+
+@table_router.post("/{branch_id}/create/{table_name}")
+async def http_create_table(branch_id: int, table_name: str):
+    pass
+
+
+@table_router.get("/{branch_id}/get/{table_id}")
+async def http_get_table(branch_id: int, table_id: int):
+    pass
+
+
+@table_router.patch("/{branch_id}/update/{table_id}/{name}")
+async def http_update_table(branch_id: int, table_id: int, name: str):
+    pass
+
+
+@table_router.patch("/{branch_id}/delete/{table_id}")
+async def http_delete_table(branch_id: int, table_id: int):
+    pass
