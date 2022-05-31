7ccc511101e23935d070c102173cc4ecd531bf24
semen603089 <semen603089@mail.ru>
2022-05-31 09:11:49 +0300

    features

--- httpengine/http_columns.py
+++ httpengine/http_columns.py
@@ -1,34 +1,39 @@
+from typing import Tuple
+
 from fastapi import APIRouter
 from . import session
 from dbengine.column import get_column, delete_column, update_column, create_column
 from dbengine.table import get_table
 from dbengine.branch import get_branch
+from dbengine.models import DbColumn, DbColumnAttributes, Commit
 
 column_router = APIRouter(prefix="/{branch_id}/column")
 
 
 @column_router.post("/create/{table_id}/{name}/{datatype}")
-async def http_create_column(branch_id: int, table_id: int, name: str, datatype: str):
+async def http_create_column(branch_id: int, table_id: int, name: str, datatype: str) -> Tuple[
+    DbColumn, DbColumnAttributes, Commit]:
     branch = get_branch(branch_id, session=session)
     table = get_table(branch, table_id, session=session)
     return create_column(branch, table[0], name=name, datatype=datatype, session=session)
 
 
 @column_router.get("/get/{column_id}")
-async def http_get_column(branch_id: int, column_id: int):
+async def http_get_column(branch_id: int, column_id: int) -> Tuple[DbColumn, DbColumnAttributes]:
     branch = get_branch(branch_id, session=session)
     return get_column(branch, column_id, session=session)
 
 
 @column_router.patch("/update/{column_id}/{name}/{datatype}")
-async def http_update_column(branch_id: int, column_id: int, name: str, datatype: str):
+async def http_update_column(branch_id: int, column_id: int, name: str, datatype: str) -> Tuple[
+    DbColumn, DbColumnAttributes, Commit]:
     branch = get_branch(branch_id, session=session)
     column = get_column(branch, column_id, session=session)
     return update_column(branch, column[0], name=name, datatype=datatype, session=session)
 
 
 @column_router.delete("/delete/{column_id}")
-async def http_delete_column(branch_id: int, column_id: int):
+async def http_delete_column(branch_id: int, column_id: int) -> Commit:
     branch = get_branch(branch_id, session=session)
     column = get_column(branch, column_id, session=session)
     return delete_column(branch, column[0], session=session)
--- httpengine/http_tables.py
+++ httpengine/http_tables.py
@@ -1,32 +1,35 @@
+from typing import Tuple
+
 from fastapi import APIRouter
 from dbengine.branch import get_branch
 from . import session
 from dbengine.table import create_table, delete_table, update_table, get_table
+from dbengine.models import DbTable, DbTableAttributes, Commit
 
 table_router = APIRouter(prefix="/{branch_id}/table")
 
 
 @table_router.post("/create/{table_name}")
-async def http_create_table(branch_id: int, table_name: str):
+async def http_create_table(branch_id: int, table_name: str) -> Tuple[DbTable, DbTableAttributes, Commit]:
     branch = get_branch(branch_id, session=session)
     return create_table(branch, table_name, session=session)
 
 
 @table_router.get("/get/{table_id}")
-async def http_get_table(branch_id: int, table_id: int):
+async def http_get_table(branch_id: int, table_id: int) -> Tuple[DbTable, DbTableAttributes]:
     branch = get_branch(branch_id, session=session)
     return get_table(branch, table_id, session=session)
 
 
 @table_router.patch("/update/{table_id}/{name}")
-async def http_update_table(branch_id: int, table_id: int, name: str):
+async def http_update_table(branch_id: int, table_id: int, name: str) -> Tuple[DbTable, DbTableAttributes, Commit]:
     branch = get_branch(branch_id, session=session)
     table = get_table(branch, table_id, session=session)
     return update_table(branch, table[0], name)
 
 
 @table_router.delete("/delete/{table_id}")
-async def http_delete_table(branch_id: int, table_id: int):
+async def http_delete_table(branch_id: int, table_id: int) -> Commit:
     branch = get_branch(branch_id, session=session)
     table = get_table(branch, table_id, session=session)
     return delete_table(branch, table[0], session=session)
