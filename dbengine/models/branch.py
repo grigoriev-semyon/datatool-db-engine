8a469255c00244db01de98cfaf246bc58a1a20ae
Roman Dyakov <dyakovri@gmail.com>
2022-06-01 12:04:02 +0300

    prev and next branches

--- dbengine/models/branch.py
+++ dbengine/models/branch.py
@@ -1,5 +1,8 @@
+from __future__ import annotations
+
 from datetime import datetime
 from enum import Enum
+from typing import List
 
 from sqlalchemy import Column, DateTime
 from sqlalchemy import Enum as EnumDb
@@ -22,12 +25,23 @@ class Branch(Base):
     name = Column(String)
     create_ts = Column(DateTime, default=datetime.utcnow, nullable=False)
 
+    def __repr__(self):
+        return f"<Branch id={self.id} type={self.type}>"
+
 
 class Commit(Base):
     id = Column(Integer, primary_key=True)
     prev_commit_id = Column(Integer, ForeignKey("commit.id"))
     dev_branch_id = Column(Integer, ForeignKey("branch.id"), default=None)
-    branch_id = Column(Integer)
+    branch_id = Column(Integer, ForeignKey("branch.id"))
     attribute_id_in = Column(Integer, ForeignKey("db_attributes.id"))
     attribute_id_out = Column(Integer, ForeignKey("db_attributes.id"))
     create_ts = Column(DateTime, default=datetime.utcnow, nullable=False)
+
+    next_commit: List[Commit]
+    prev_commit: Commit = relationship("Commit", foreign_keys=[prev_commit_id], backref="next_commit", remote_side=id)
+
+    branch: Branch = relationship("Branch", foreign_keys=[branch_id])
+
+    def __repr__(self):
+        return f"<Commit id={self.id} branch_id={self.branch_id}>"
--- tests/test_branch.py
+++ tests/test_branch.py
@@ -49,3 +49,18 @@ def test_branch_mr():
     unrequest_merge_branch(branch, session=session)
     assert branch.type == BranchTypes.WIP
     create_table(branch, "Test Branch 2", session=session)
+
+
+def test_commit_iteration():
+    session = Session()
+    branch = create_branch("Test 3", session=session)
+    _, _, commit1 = create_table(branch, "Test 3 Table 1", session=session)
+    _, _, commit2 = create_table(branch, "Test 3 Table 2", session=session)
+    session.flush()
+
+    assert commit2.prev_commit == commit1
+    assert commit2 in commit1.next_commit
+    assert commit1.branch == commit2.branch == branch
+
+    # First commit in branch is empty commit
+    assert commit1.prev_commit.prev_commit.branch.type == BranchTypes.MAIN
