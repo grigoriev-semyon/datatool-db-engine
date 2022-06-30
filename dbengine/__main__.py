6651e243577a97d150bd2d7b67d8383c962e0f47
semen603089 <semen603089@mail.ru>
2022-06-30 16:27:01 +0300

    optimize code

--- dbengine/__main__.py
+++ dbengine/__main__.py
@@ -1,9 +1,7 @@
 import uvicorn
 
 from dbengine import app
-from dbengine.db_connector.db_connector import PostgreConnector, Settings
 
 
 if __name__ == '__main__':
     uvicorn.run(app)
-    settings = Settings()
